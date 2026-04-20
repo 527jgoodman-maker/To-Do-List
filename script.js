94de6a983f694e9c0c30d7946c23145509c021f2
527jgoodman-maker <527JGoodman@frhsd.com>
2026-04-20 10:36:47 -0400

    started the to do list demo

---
+++ script.js
@@ -0,0 +1,33 @@
+let tasks = []
+
+document.getElementById("addTaskBtn").addEventListener("click", function() {
+
+    let taskInput = document.getElementById("taskInput").value 
+    
+    if (taskInput) {
+        tasks.push(taskInput)
+
+        document.getElementById("taskInput").value = ""
+
+        displayTasks()
+    }
+})
+
+function displayTasks() {
+    let taskList = document.getElementById("taskList")
+    
+    taskList.innerHTML = ""
+    
+    tasks.forEach((task, index) => {
+        let li = document.createElement("li")
+        li.classList.add(
+            "list-group-item", 
+            "d-flex", 
+            "justify-content-between", 
+            "align-items-center"
+        )
+        li.innerHTML = `${task}` + `<button class="btn btn-success btn-sm" onclick="removeTask('${index}')">✓</button>`
+        
+        taskList.appendChild(li)
+    })
+}
